dcec47facd41ab80d43d552c698f4d380effb01e
Eana Hufwe <ilove@1a23.com>
2020-07-28 22:16:16 +0800

    fix: inline style for databox and textbox

--- next/components/entryMediaTypes/TextBox.tsx
+++ next/components/entryMediaTypes/TextBox.tsx
@@ -1,5 +1,5 @@
 import React, { ReactNode } from "react";
-import { makeStyles, CardMedia } from "@material-ui/core";
+import { CardMedia } from "@material-ui/core";
 
 type TextBoxProps = {
   className?: string,
@@ -11,23 +11,21 @@ type TextBoxProps = {
 };
 
 export function TextBox(props: TextBoxProps) {
-  const styleSet = makeStyles({
-    root: {
-      display: "flex",
-      justifyContent: "center",
-      alignItems: "center",
-      lineHeight: 1,
-      textAlign: "center",
-      whiteSpace: "pre-wrap",
-      fontSize: props.fontSize,
-      fontWeight: parseInt(props.fontWeight || "500"),
-      backgroundColor: props.backgroundColor,
-      color: props.color,
-    },
-  })();
   return (
     <CardMedia
-      className={`${props.className} ${styleSet.root}`}
+      className={`${props.className}`}
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        lineHeight: 1,
+        textAlign: "center",
+        whiteSpace: "pre-wrap",
+        fontSize: props.fontSize,
+        fontWeight: parseInt(props.fontWeight || "500"),
+        backgroundColor: props.backgroundColor,
+        color: props.color,
+      }}
       component="div"
     >
       {props.children}
